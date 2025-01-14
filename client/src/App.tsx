1a045acccdd473bf6bd1e49fdd5c8455c04994c0
Jorge Vivas <yorch.vivas@gmail.com>
2025-01-14 21:53:26 +0000

    Assistant checkpoint: Add useLocation import
    Assistant generated file changes:
    - client/src/App.tsx: Import useLocation from wouter
    
    ---
    
    User prompt:
    
    error: `useLocation is not defined`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { DndProvider } from 'react-dnd';
